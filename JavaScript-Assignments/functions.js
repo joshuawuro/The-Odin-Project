f5b269665f61c1b74c38b58f5fd002f276963ff9
Joshua Awuro <joshuawuro@gmail.com>
2024-06-02 02:04:40 -0700

    done

---
+++ JavaScript-Assignments/functions.js
@@ -0,0 +1,23 @@
+add7 = (number) => {
+    return number + 7;
+}
+
+console.log(add7(4));
+
+multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+console.log(multiply(2, 4));
+
+school = (university) => {
+    return university.toUpperCase;
+}
+
+console.log(school('pentecost university'));
+
+lastLetter = (favoriteFood) => {
+    return favoriteFood.length(-1);
+} 
+
+console.log('appiah agyei');
---
+++ JavaScript-Assignments/pyramid.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 5; i++){
+    console.log(i);
+    for (j = 0; j < 5; j++){
+        console.log(j);
+    }
+}
